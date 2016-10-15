51d0a0590b785433a362748a8b4d7f4ef7f11444
Jason Miller <stormbeta@gmail.com>
2016-10-15 15:44:44 -0600

    Make record script actually usable

---
+++ readme.md
@@ -0,0 +1,7 @@
+#Audiobook encoding for personal backups
+
+Eventually intended for use on a Raspberry Pi
+
+Read audio from line in and encode it with opus
+
+`./record.sh HH:MM:SS OUTPUT.opus`
--- record.sh
+++ record.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-arecord -D hw -c 2 -r 44100 -f S32_LE | ffmpeg -i - -acodec pcm_f32le -ar 44100 "${1:-output.wav}"
+#TODO: try using sysdefault audio device instead
+
+BOOK_SECONDS="$(echo "$1" | perl -pe 's/(\d+):(\d+):(\d+)/\1*3600+\2*60+\3/' | bc)"
+
+#Opus requires floating-point pcm, but the ancient optiplex only does S32_LE?
+timeout "${BOOK_SECONDS}s" \
+    arecord -D hw -c 2 -r 44100 -f S32_LE \
+  | ffmpeg -i - -acodec pcm_f32le -ar 44100 -f wav - \
+  | opusenc --downmix-mono - "${2:-output.opus}"
