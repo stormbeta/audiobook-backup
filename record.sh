fb65e9c55201a7c04a912f7038c044b1f0aacd02
Jason Miller <stormbeta@gmail.com>
2016-10-20 10:58:00 -0600

    Add raw flags for opus encoder

--- record.sh
+++ record.sh
@@ -12,25 +12,31 @@ OUTPUT_NAME="${2:-output.opus}"
 
 echo "Book length ${BOOK_LENGTH} at ${SPEED}x speed is ${RECORD_SECONDS} seconds"
 SAMPLE_RATE='44100'
+CHANNELS='2'
 
 mode="$(echo "${OUTPUT_NAME}" | grep -oP '\w{4}$')"
 
 case $mode in
   flac)
+    #TODO: will exit early due to non-raw format
     arecord --duration=${RECORD_SECONDS} \
             --format=S24_3LE \
-            --channels=2 \
+            --channels=${CHANNELS} \
             --rate=${SAMPLE_RATE} \
-            --file-type raw \
       | flac - -f -o "${OUTPUT_NAME}"
     ;;
   opus)
     arecord --duration=${RECORD_SECONDS} \
             --format=FLOAT_LE \
-            --channels=2 \
+            --channels=${CHANNELS} \
             --file-type raw \
             --rate=${SAMPLE_RATE} \
-      | opusenc --downmix-mono - "${OUTPUT_NAME}"
+      | opusenc --downmix-mono \
+            --raw \
+            --raw-rate ${SAMPLE_RATE} \
+            --raw-chan ${CHANNELS} \
+            --raw-bits 32 \ #TODO: Verify this is correct for FLOAT_LE
+            - "${OUTPUT_NAME}"
     ;;
   *)
     echo "unknown mode ${mode}"
