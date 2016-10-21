e109d75588d5dc760d3b280d9ea90a4a2d3f45f4
Jason Miller <stormbeta@gmail.com>
2016-10-20 20:21:07 -0700

    Update to work with Raspberry Pi USB audio

--- record.sh
+++ record.sh
@@ -5,14 +5,14 @@ if [[ $# -le 1 ]]; then
   exit 1
 fi
 
-BOOK_LENGTH="$(echo "$1" | perl -pe 's/(\d+):(\d+):(\d+)/\1*3600+\2*60+\3/' | bc)"
-SPEED="${3:-1.0}"
-RECORD_SECONDS="$(echo "$BOOK_LENGTH / $SPEED" | bc)"
-OUTPUT_NAME="${2:-output.opus}"
+export BOOK_LENGTH="$(echo "$1" | perl -pe 's/(\d+):(\d+):(\d+)/\1*3600+\2*60+\3/' | bc)"
+export SPEED="${3:-1.0}"
+export RECORD_SECONDS="$(echo "$BOOK_LENGTH / $SPEED" | bc)"
+export OUTPUT_NAME="${2:-output.opus}"
 
 echo "Book length ${BOOK_LENGTH} at ${SPEED}x speed is ${RECORD_SECONDS} seconds"
-SAMPLE_RATE='44100'
-CHANNELS='2'
+export SAMPLE_RATE='44100'
+export CHANNELS='1'
 
 mode="$(echo "${OUTPUT_NAME}" | grep -oP '\w{4}$')"
 
@@ -27,7 +27,7 @@ case $mode in
     ;;
   opus)
     arecord --duration=${RECORD_SECONDS} \
-            --format=FLOAT_LE \
+            --format=S16_LE \
             --channels=${CHANNELS} \
             --file-type raw \
             --rate=${SAMPLE_RATE} \
@@ -35,7 +35,7 @@ case $mode in
             --raw \
             --raw-rate ${SAMPLE_RATE} \
             --raw-chan ${CHANNELS} \
-            --raw-bits 32 \ #TODO: Verify this is correct for FLOAT_LE
+            --raw-bits 16 \
             - "${OUTPUT_NAME}"
     ;;
   *)
