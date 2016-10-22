d61ad9e69f35db5359a2bee5bfd1476eccdb6b1d
Jason Miller <stormbeta@gmail.com>
2016-10-22 17:36:28 -0600

    Rename to book title and author, use m4a extension instead of mp4

--- backup.sh
+++ backup.sh
@@ -5,9 +5,10 @@ OUTPUT_DIR=''
 
 for BOOK in $@; do
   echo "Book: ${BOOK}"
-  BOOK_MP4="$(echo "$(basename "${BOOK}")" | sed -r 's/aax$/mp4/')"
+  BOOK_M4A="$(echo "$(basename "${BOOK}")" | sed -r 's/aax$/m4a/')"
   BOOK_TITLE="$(ffmpeg -i "${BOOK}" 2>&1 | grep '^    title' | grep -Po '(?<=: ).*' | sed -r 's/ \(Unabridged\)$//')"
-  ffmpeg -activation_bytes ${ACTIVATION_BYTES} -i "${BOOK}" -vn -c:a copy "${OUTPUT_DIR}/${BOOK_MP4}" && \
-    mv "${OUTPUT_DIR}/${BOOK_MP4}" "${OUTPUT_DIR}/${BOOK_TITLE}.mp4" && \
+  BOOK_AUTHOR="$(ffmpeg -i "${BOOK}" 2>&1 | grep '^    artist ' | grep -oP '(?<=: ).*')"
+  ffmpeg -activation_bytes ${ACTIVATION_BYTES} -i "${BOOK}" -vn -c:a copy "${OUTPUT_DIR}/${BOOK_M4A}" && \
+    mv "${OUTPUT_DIR}/${BOOK_M4A}" "${OUTPUT_DIR}/${BOOK_TITLE} - ${BOOK_AUTHOR}.m4a" && \
     rm "${BOOK}"
 done
