f56d9646c0cc2f90da2b24db9f2d4417212fe8a6
Jason Miller <stormbeta@gmail.com>
2016-10-29 01:13:24 -0600

    Strip first 2 seconds to remove annoying vendor message

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ for BOOK in $@; do
   BOOK_M4A="$(echo "$(basename "${BOOK}")" | sed -r 's/aax$/m4a/')"
   BOOK_TITLE="$(ffmpeg -i "${BOOK}" 2>&1 | grep '^    title' | grep -Po '(?<=: ).*' | sed -r 's/ \(Unabridged\)$//')"
   BOOK_AUTHOR="$(ffmpeg -i "${BOOK}" 2>&1 | grep '^    artist ' | grep -oP '(?<=: ).*')"
-  ffmpeg -activation_bytes ${ACTIVATION_BYTES} -i "${BOOK}" -vn -c:a copy "${OUTPUT_DIR}/${BOOK_M4A}" && \
+  ffmpeg -ss 2 -activation_bytes ${ACTIVATION_BYTES} -i "${BOOK}" -vn -c:a copy "${OUTPUT_DIR}/${BOOK_M4A}" && \
     mv "${OUTPUT_DIR}/${BOOK_M4A}" "${OUTPUT_DIR}/${BOOK_TITLE} - ${BOOK_AUTHOR}.m4a" && \
     rm "${BOOK}"
 done
