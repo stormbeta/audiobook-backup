f92f3a585b7aa157c45dc0f83ad6578536c37c23
Jason Miller <stormbeta@gmail.com>
2016-10-29 01:15:11 -0600

    Don't automatically delete AAX file

--- backup.sh
+++ backup.sh
@@ -9,6 +9,6 @@ for BOOK in $@; do
   BOOK_TITLE="$(ffmpeg -i "${BOOK}" 2>&1 | grep '^    title' | grep -Po '(?<=: ).*' | sed -r 's/ \(Unabridged\)$//')"
   BOOK_AUTHOR="$(ffmpeg -i "${BOOK}" 2>&1 | grep '^    artist ' | grep -oP '(?<=: ).*')"
   ffmpeg -ss 2 -activation_bytes ${ACTIVATION_BYTES} -i "${BOOK}" -vn -c:a copy "${OUTPUT_DIR}/${BOOK_M4A}" && \
-    mv "${OUTPUT_DIR}/${BOOK_M4A}" "${OUTPUT_DIR}/${BOOK_TITLE} - ${BOOK_AUTHOR}.m4a" && \
-    rm "${BOOK}"
+    mv "${OUTPUT_DIR}/${BOOK_M4A}" "${OUTPUT_DIR}/${BOOK_TITLE} - ${BOOK_AUTHOR}.m4a"
+    #rm "${BOOK}"
 done
