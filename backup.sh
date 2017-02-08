c0431d3463720817fec0fa55b2d76b66a3aa0abf
Jason Miller <stormbeta@gmail.com>
2017-02-08 10:01:59 -0700

    Add optional section (disabled) for dropbox upload

--- backup.sh
+++ backup.sh
@@ -8,7 +8,20 @@ for BOOK in $@; do
   BOOK_M4A="$(echo "$(basename "${BOOK}")" | sed -r 's/aax$/m4a/')"
   BOOK_TITLE="$(ffmpeg -i "${BOOK}" 2>&1 | grep '^    title' | grep -Po '(?<=: ).*' | sed -r 's/ \(Unabridged\)$//')"
   BOOK_AUTHOR="$(ffmpeg -i "${BOOK}" 2>&1 | grep '^    artist ' | grep -oP '(?<=: ).*')"
-  ffmpeg -ss 2 -activation_bytes ${ACTIVATION_BYTES} -i "${BOOK}" -vn -c:a copy "${OUTPUT_DIR}/${BOOK_M4A}" && \
-    mv "${OUTPUT_DIR}/${BOOK_M4A}" "${OUTPUT_DIR}/${BOOK_TITLE} - ${BOOK_AUTHOR}.m4a"
-    #rm "${BOOK}"
+  OUTPUT_NAME="${BOOK_TITLE} - ${BOOK_AUTHOR}.m4a"
+  echo "OUTPUT: ${OUTPUT_DIR}/${OUTPUT_NAME}"
+  ffmpeg -ss 2 -activation_bytes "${ACTIVATION_BYTES}" -i "${BOOK}" -vn -c:a copy "${OUTPUT_DIR}/${BOOK_M4A}"
+  mv "${OUTPUT_DIR}/${BOOK_M4A}" "${OUTPUT_DIR}/${OUTPUT_NAME}"
+  #if [[ "$(uname -s)" == "Darwin" ]] && command -v dropbox; then
+    #(
+      #cd "${OUTPUT_DIR}"
+      ##uses https://github.com/andreafabrizi/Dropbox-Uploader
+      #dropbox upload "${OUTPUT_NAME}" "Archive/Audiobooks/${OUTPUT_NAME}" &
+      #DROPBOX_PID="$!"
+      #echo "DROPBOX_PID: ${DROPBOX_PID}"
+      #trap "kill ${DROPBOX_PID}; pkill -f curl.*dropbox" SIGINT SIGTERM
+      ##NOTE: Mac only - ensures laptop won't sleep while uploading
+      #caffeinate -w "${DROPBOX_PID}"
+    #)
+  #fi
 done
