1df7806ad3f044fa36707b86dec472cfff1ffa0d
Jason Miller <stormbeta@gmail.com>
2017-07-13 00:07:20 -0600

    Initial Downpour script
    Doesn't download anything, just properly concatentates the pieces
    For some inane reason, Downpour doesn't allow you to download a simple
    single file, and instead insists on pointlessly splitting up even small
    books into separate manual downloads, which is then quite tedious to
    manage.

---
+++ downpour.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# TODO: Download metadata automatically. Unfortunately it's not baked into the files downpour gives us
+NAME="$2"
+AUTHOR="$3"
+
+# TODO: These must be manually downloaded 
+ls -q ${1}*.m4b | sed -r "s/^(.*)/file '\1'/g" > input
+
+# Bizarrely, ffmpeg cannot handle album art, so we have to add it back in ourselves
+# See: https://trac.ffmpeg.org/ticket/2798
+artwork="$(AtomicParsley $(ls -q ${1}*.m4b | head -n 1) --extractPix | grep -oP '(?<= )[^ ]*\.(jpg|png)$')"
+
+ffmpeg -f concat -safe 0 -i input -vn -c copy output.m4a
+
+AtomicParsley output.m4a --author "${AUTHOR}" --artwork "${artwork}" --title "${NAME}"
+
+OUTPUT_NAME="${NAME} - ${AUTHOR}.m4a"
+OUTPUT_DIR='/tmp'
+mv output-temp*.m4a "/tmp/${OUTPUT_NAME}"
+
