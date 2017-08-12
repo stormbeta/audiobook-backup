d0b317a022aec89bf91008c48da18c743037c2f8
Jason Miller <stormbeta@gmail.com>
2017-08-11 20:04:04 -0600

    Revert "Smarter downpour script that uses a real tmpdir"
    This reverts commit fe7cecb7151b237efefe878a4b91e9934de776dc.

--- downpour.sh
+++ downpour.sh
@@ -1,40 +1,32 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+# TODO: These must be manually downloaded 
+ls -q ${1}*.m4b | sed -r "s/^(.*)/file '\1'/g" > input
+FILE1="$(ls -q ${1}*.m4b | head -n 1)"
 
-TMPDIR="$(mktemp -d)"
-echo "${TMPDIR}"
-trap "rm -rf '${TMPDIR}'" SIGINT SIGTERM EXIT
+fields="$(AtomicParsley "${FILE1}" -t | sed -r 's/^Atom "(.*+)" contains: (.*)$/\1=\2/g')"
 
-(
-  cd ${TMPDIR}
-  # TODO: These must be manually downloaded
-  ls -q ${1}*.m4b | sed -r "s/^(.*)/file '\1'/g" > input
-  FILE1="$(ls -q ${1}*.m4b | head -n 1)"
+AUTHOR="$(echo -e "${fields}" | grep 'aART' | sed -r 's/^aART=(.*)$/\1/')"
+NAME="$(echo -e "${fields}" | grep '©alb' | perl -pe 's/^©alb=(.*?)($| \(Unabridged\))/\1/')" 
+ENC="$(echo -e "${fields}" | grep '©enc' | sed -r 's/^©enc=(.*)$/\1/')"
+PUBLISHER="$(echo -e "${fields}" | grep 'cprt' | sed -r 's/^cprt=(.*)$/\1/')"
+echo "NAME: ${NAME}"
+echo "AUTHOR: ${AUTHOR}"
 
-  fields="$(AtomicParsley "${FILE1}" -t | sed -r 's/^Atom "(.*+)" contains: (.*)$/\1=\2/g')"
+# Bizarrely, ffmpeg cannot handle album art, so we have to add it back in ourselves
+# See: https://trac.ffmpeg.org/ticket/2798
+artwork="$(AtomicParsley $(ls -q ${1}*.m4b | head -n 1) --extractPix | grep -oP '(?<= )[^ ]*\.(jpg|png)$')"
 
-  AUTHOR="$(echo -e "${fields}" | grep 'aART' | sed -r 's/^aART=(.*)$/\1/')"
-  NAME="$(echo -e "${fields}" | grep '©alb' | perl -pe 's/^©alb=(.*?)($| \(Unabridged\))/\1/')" 
-  ENC="$(echo -e "${fields}" | grep '©enc' | sed -r 's/^©enc=(.*)$/\1/')"
-  PUBLISHER="$(echo -e "${fields}" | grep 'cprt' | sed -r 's/^cprt=(.*)$/\1/')"
-  echo "NAME: ${NAME}"
-  echo "AUTHOR: ${AUTHOR}"
+ffmpeg -f concat -safe 0 -i input -vn -c copy output.m4a
 
-  # Bizarrely, ffmpeg cannot handle album art, so we have to add it back in ourselves
-  # See: https://trac.ffmpeg.org/ticket/2798
-  artwork="$(AtomicParsley $(ls -q ${1}*.m4b | head -n 1) --extractPix | grep -oP '(?<= )[^ ]*\.(jpg|png)$')"
+AtomicParsley output.m4a \
+  --title "${NAME}" \
+  --artist "${AUTHOR}" \
+  --artwork "${artwork}" \
+  --copyright "${PUBLISHER}" \
+  --encodedBy "${ENC}"
 
-  ffmpeg -f concat -safe 0 -i input -vn -c copy output.m4a
+OUTPUT_NAME="${NAME} - ${AUTHOR}.m4a"
+OUTPUT_DIR='/tmp'
+mv output-temp*.m4a "/tmp/${OUTPUT_NAME}"
 
-  AtomicParsley output.m4a \
-    --title "${NAME}" \
-    --artist "${AUTHOR}" \
-    --artwork "${artwork}" \
-    --copyright "${PUBLISHER}" \
-    --encodedBy "${ENC}"
-
-  OUTPUT_NAME="${NAME} - ${AUTHOR}.m4a"
-  OUTPUT_PATH="${PWD}/${OUTPUT_NAME}"
-  mv output-temp*.m4a "${OUTPUT_PATH}"
-)
