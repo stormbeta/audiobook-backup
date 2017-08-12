fe7cecb7151b237efefe878a4b91e9934de776dc
Jason Miller <stormbeta@gmail.com>
2017-08-11 19:18:11 -0600

    Smarter downpour script that uses a real tmpdir

--- downpour.sh
+++ downpour.sh
@@ -1,32 +1,40 @@
 #!/usr/bin/env bash
 
-# TODO: These must be manually downloaded 
-ls -q ${1}*.m4b | sed -r "s/^(.*)/file '\1'/g" > input
-FILE1="$(ls -q ${1}*.m4b | head -n 1)"
+set -eo pipefail
 
-fields="$(AtomicParsley "${FILE1}" -t | sed -r 's/^Atom "(.*+)" contains: (.*)$/\1=\2/g')"
+TMPDIR="$(mktemp -d)"
+echo "${TMPDIR}"
+trap "rm -rf '${TMPDIR}'" SIGINT SIGTERM EXIT
 
-AUTHOR="$(echo -e "${fields}" | grep 'aART' | sed -r 's/^aART=(.*)$/\1/')"
-NAME="$(echo -e "${fields}" | grep '©alb' | perl -pe 's/^©alb=(.*?)($| \(Unabridged\))/\1/')" 
-ENC="$(echo -e "${fields}" | grep '©enc' | sed -r 's/^©enc=(.*)$/\1/')"
-PUBLISHER="$(echo -e "${fields}" | grep 'cprt' | sed -r 's/^cprt=(.*)$/\1/')"
-echo "NAME: ${NAME}"
-echo "AUTHOR: ${AUTHOR}"
+(
+  cd ${TMPDIR}
+  # TODO: These must be manually downloaded
+  ls -q ${1}*.m4b | sed -r "s/^(.*)/file '\1'/g" > input
+  FILE1="$(ls -q ${1}*.m4b | head -n 1)"
 
-# Bizarrely, ffmpeg cannot handle album art, so we have to add it back in ourselves
-# See: https://trac.ffmpeg.org/ticket/2798
-artwork="$(AtomicParsley $(ls -q ${1}*.m4b | head -n 1) --extractPix | grep -oP '(?<= )[^ ]*\.(jpg|png)$')"
+  fields="$(AtomicParsley "${FILE1}" -t | sed -r 's/^Atom "(.*+)" contains: (.*)$/\1=\2/g')"
 
-ffmpeg -f concat -safe 0 -i input -vn -c copy output.m4a
+  AUTHOR="$(echo -e "${fields}" | grep 'aART' | sed -r 's/^aART=(.*)$/\1/')"
+  NAME="$(echo -e "${fields}" | grep '©alb' | perl -pe 's/^©alb=(.*?)($| \(Unabridged\))/\1/')" 
+  ENC="$(echo -e "${fields}" | grep '©enc' | sed -r 's/^©enc=(.*)$/\1/')"
+  PUBLISHER="$(echo -e "${fields}" | grep 'cprt' | sed -r 's/^cprt=(.*)$/\1/')"
+  echo "NAME: ${NAME}"
+  echo "AUTHOR: ${AUTHOR}"
 
-AtomicParsley output.m4a \
-  --title "${NAME}" \
-  --artist "${AUTHOR}" \
-  --artwork "${artwork}" \
-  --copyright "${PUBLISHER}" \
-  --encodedBy "${ENC}"
+  # Bizarrely, ffmpeg cannot handle album art, so we have to add it back in ourselves
+  # See: https://trac.ffmpeg.org/ticket/2798
+  artwork="$(AtomicParsley $(ls -q ${1}*.m4b | head -n 1) --extractPix | grep -oP '(?<= )[^ ]*\.(jpg|png)$')"
 
-OUTPUT_NAME="${NAME} - ${AUTHOR}.m4a"
-OUTPUT_DIR='/tmp'
-mv output-temp*.m4a "/tmp/${OUTPUT_NAME}"
+  ffmpeg -f concat -safe 0 -i input -vn -c copy output.m4a
 
+  AtomicParsley output.m4a \
+    --title "${NAME}" \
+    --artist "${AUTHOR}" \
+    --artwork "${artwork}" \
+    --copyright "${PUBLISHER}" \
+    --encodedBy "${ENC}"
+
+  OUTPUT_NAME="${NAME} - ${AUTHOR}.m4a"
+  OUTPUT_PATH="${PWD}/${OUTPUT_NAME}"
+  mv output-temp*.m4a "${OUTPUT_PATH}"
+)
