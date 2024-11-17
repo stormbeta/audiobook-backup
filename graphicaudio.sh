63254c23afdaf3fa76a92f09cc7b91adc4085a33
Jason Miller <git@stormbeta.dev>
2024-11-17 16:44:16 -0700

    Improve graphic audio script
    * Works with older graphic audio titles that aren't in subdirectory
    * Pulls title from file (NOTE: not always accurate!)
    * Uses tmp directory instead of current path
    
    TODO: remove colon in titles as some filesystems would choke on it

--- graphicaudio.sh
+++ graphicaudio.sh
@@ -3,26 +3,36 @@
 set -eo pipefail
 shopt -qs extglob
 
+# Graphic Audio is already DRM-free, and the files large enough that it rarely
+# makes sense to combine them. We only need to unzip and rename.
+
 if [[ $# -lt 1 ]]; then
-  echo "Usage: ${0} GRAPHIC_AUDIO_ZIPFILE" 1>&2
+  echo "Usage: ${0} GRAPHIC_AUDIO_ZIPFILES..." 1>&2
   exit 1
 fi
 
-readonly zipfile="$1"
-readonly author="Brandon Sanderson"
-
 if [[ -d /mnt/dropbox/Archive/Audiobooks ]]; then
-  output="/mnt/dropbox/Archive/Audiobooks/Graphic Audio"
+  readonly output="/mnt/dropbox/Archive/Audiobooks/Graphic Audio"
 elif [[ -d /Volumes/dropbox/Archive/Audiobooks ]]; then
-  output="/Volumes/dropbox/Archive/Audiobooks/Graphic Audio"
+  readonly output="/Volumes/dropbox/Archive/Audiobooks/Graphic Audio"
 else
   echo "dropbox mount not found on /mnt or /Volumes" 1>&2
   exit 2
 fi
 
-readonly title="$(zipinfo -1 "$zipfile" | head -n1 | sed -E 's:/::g')"
+# Graphic Audio does not cleanly embed author name in their books - have to set manually
+author="Brent Weeks"
 
-unzip "$zipfile"
-echo "Copying M4B file to archival..." 1>&2
-mv "${title}/"*.m4b "${output}/${title} - ${author}.m4b"
-rmdir "${title}"
+for zipfile in $@; do
+  filename="$(zipinfo -1 "$zipfile" | grep -E '\.m4b$')"
+  unzip "$zipfile" -d /tmp
+  echo "FILENAME: $filename" 1>&2
+  (
+    cd /tmp
+    title="$(ffprobe -v error -show_entries format_tags=album -of default=noprint_wrappers=1:nokey=1 "$filename" \
+      | perl -pe 's/(\d of \d)/Graphic Audio \1/g')"
+    output_path="${output}/${title} - ${author}.m4b"
+    echo "Copying M4B file to archival: ${output_path}" 1>&2
+    mv "$filename" "${output_path}"
+  )
+done
