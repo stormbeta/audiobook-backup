357f4f7c5c2679d530dd504c2ae0a9ac90a9e8e2
Jason Miller <git@stormbeta.dev>
2026-06-28 23:26:49 -0600

    graphicaudio.sh improvements
    * Auto-detect author from file metadata
    * Cleanup temp files

--- graphicaudio.sh
+++ graphicaudio.sh
@@ -1,48 +1,49 @@
 #!/usr/bin/env bash
 
 set -eo pipefail
-shopt -qs extglob
 
 # Graphic Audio is already DRM-free, and the files large enough that it rarely
 # makes sense to combine them. We only need to unzip and rename.
 
 function usage {
-  # Graphic Audio does not cleanly embed author name in their books - have to pass manually
-  echo "Usage: ${0} 'AUTHOR' GRAPHIC_AUDIO_ZIPFILES..." 1>&2
+  echo "Usage: ${0} GRAPHIC_AUDIO_ZIPFILES..." 1>&2
 }
 
-if [[ $# -lt 2 ]]; then
+if [[ $# -lt 1 ]]; then
   usage
   exit 1
 fi
 
-if [[ -f "$1" ]]; then
-  echo "ERROR: First argument should be quoted author name, but it looks like a file path, aborting" 1>&2
-  usage
-  exit 3
-fi
-
 source config.sh
 
-author="$1"
-shift 1
+output="${OUTPUT_DIR}/Graphic Audio"
+tmpdir="$(mktemp -d)"
+trap 'rm -rf "$tmpdir"' EXIT
 
-export output="$OUTPUT_DIR"
-
-for zipfile in $@; do
+for zipfile in "$@"; do
   if [[ ! -f "$zipfile" ]]; then
     echo "'$zipfile' not found! Aborting" 1>&2
     exit 4
   fi
+
   filename="$(zipinfo -1 "$zipfile" | grep -E '\.m4b$')"
-  unzip "$zipfile" -d /tmp
+  unzip -q "$zipfile" -d "$tmpdir"
+  filepath="$tmpdir/$filename"
   echo "FILENAME: $filename" 1>&2
-  (
-    cd /tmp
-    title="$(ffprobe -v error -show_entries format_tags=album -of default=noprint_wrappers=1:nokey=1 "$filename" \
-      | perl -pe 's/(\d of \d)/Graphic Audio \1/;s/:/ -/g')"
-    output_path="${output}/${title} - ${author}.m4b"
-    echo "Copying M4B file to archival: ${output_path}" 1>&2
-    mv "$filename" "${output_path}"
-  )
+
+  title="$(ffprobe -v error -show_entries format_tags=album -of default=noprint_wrappers=1:nokey=1 "$filepath" \
+    | perl -pe 's/(\d of \d)/Graphic Audio \1/;s/:/ -/g')"
+  echo "TITLE: ${title}" 1>&2
+
+  copyright="$(ffprobe -v error -show_entries format_tags=copyright -of default=noprint_wrappers=1:nokey=1 "$filepath")"
+  author="$(echo "$copyright" | perl -ne 'print "$1" if /Copyright © \d+ (.+?)\. All rights/i')"
+  if [[ -z "$author" ]]; then
+    echo "ERROR: could not parse author from copyright tag: '$copyright'" 1>&2
+    exit 5
+  fi
+  echo "AUTHOR: $author" 1>&2
+
+  output_path="${output}/${title} - ${author}.m4b"
+  echo "Copying M4B file to archival: ${output_path}" 1>&2
+  mv "$filepath" "${output_path}"
 done
